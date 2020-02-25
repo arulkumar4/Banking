084b5edc88471aa682374f32043deffcf27707ae
deril-27051998 <57354775+deril-27051998@users.noreply.github.com>
2020-02-25 13:15:00 +0530

    City Module Completed(CRUD)

---
+++ Banking.Business.Models/Bank/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Banking.Business.Models
+{
+    public class Employee
+    {
+        public Employee() : this(0) { }
+        public Employee(int employeeId) => EmployeeId = employeeId;
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset Dob { get; set; }
+        public List<Address> AddressList { get; set; }
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+    }
+}
