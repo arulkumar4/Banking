f603487a01eb3254e5f0900456a2ef961c94b971
GayathriMukesh <57354099+GayathriMukesh@users.noreply.github.com>
2020-03-06 12:31:00 +0530

    Tables and StoreProcedures Are Added

---
+++ Banking.Database/Account/Stored Procedures/UpdateCustomerDetails_1.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [Account].[UpdateCustomerDetails]
+(
+	@CustomerId VARCHAR(20),
+	@Password VARCHAR(100),
+	@FirstName DataTypeName,
+	@LastName DataTypeName,
+	@Address DataTypeAddress,      
+	@ContactNumber DataTypeContactNumber,
+	@Mail DataTypeMail
+)
+AS
+BEGIN 
+	IF EXISTS (SELECT 'True' FROM [Account].[Account] WHERE @CustomerId = CustomerId 
+			   AND (SUBSTRING(master.dbo.fn_varbintohexstr(HashBytes('MD5', @Password)), 1, 100)= Password))
+	  BEGIN 
+		UPDATE [Account].[Customer]
+		SET FirstName=@FirstName, LastName=@LastName, 
+		FullName=CONCAT(@LastName,' ',@FirstName),
+		Address= @Address, ContactNumber=@ContactNumber, Mail=@Mail 
+		WHERE Id= @CustomerId
+		--PRINT 'Updated Successfully !!!'
+		SELECT 'Updated Successfully !!!'
+	  END
+	ELSE
+		SELECT 'Invalid Customer ID / Password !!!!'
+END
--- Banking.Database/Security/Transaction.sql
+++ Banking.Database/Security/Transaction.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [Transaction]
     AUTHORIZATION [dbo];
 
+
+
