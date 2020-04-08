e0383f24a304c891174e2d4ea5a3948bc5f08529
GayathriMukesh <57354099+GayathriMukesh@users.noreply.github.com>
2020-04-08 15:01:58 +0530

    Missed some files and attached those

--- Banking.NgWeb/src/app/app-routing.module.ts
+++ Banking.NgWeb/src/app/app-routing.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerDashboardComponent } from './customer/customer-dashboard/customer-dashboard.component';
-import { CustomerRegisteredDetailsComponent } from './customer/customer-registered-details/customer-registered-details.component';
-
+import { CustomerAccountComponent } from './customer/customer-account/customer-account.component';
 
 const routes: Routes = [
-  {path:'adminhomepage',component:CustomerComponent,children:
-  [
-  {path:'',redirectTo:'home',pathMatch:'full'},  
-  // {path:'home',component:HomeComponet},
-  {path:'customer_Register',component:CustomerRegisteredDetailsComponent},
-  {path:'customer_Dashboard',component:CustomerDashboardComponent}
-  ]
-  }
+  { path: '', redirectTo: 'customerDashboard', pathMatch: 'full' },
+  { path: 'customer', component: CustomerComponent },
+  { path: 'customerDashboard', component: CustomerDashboardComponent },
+  { path: 'customerAccount', component: CustomerAccountComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
